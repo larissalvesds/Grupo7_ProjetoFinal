95bdddaf2cea3f6ed10d0304a4c45b3494135ac9
Chayenne Pereira <chayennep4@gmail.com>
2025-10-05 17:46:06 -0300

    Update Grupo3_7_PROJETOFINAL - Inclusão LGBTQIA+.sql

--- sql/Grupo3_7_PROJETOFINAL - Inclusão LGBTQIA+.sql
+++ sql/Grupo3_7_PROJETOFINAL - Inclusão LGBTQIA+.sql
@@ -2,6 +2,7 @@ SELECT
   apoioLGBTQIA,
   fazparteLGBTQIA,
   COUNT(*) AS total
-FROM `t1engenhariadados.Grupo3_7_PROJETOFINAL.tabelafinal`
+FROM `t1engenhariadados.Grupo3_7_PROJETOFINAL.tabelafinal_atualizada`
 GROUP BY apoioLGBTQIA, fazparteLGBTQIA
+
 ORDER BY total DESC;
